43d8db4ea4c9609c3c012d9f70e2944548d544f1
Anjali-24jan1998 <anjaliraghuvanshi7066@gmail.com>
2022-05-20 14:28:11 +0530

    adding java files

---
+++ arrayforeach.java
@@ -0,0 +1,16 @@
+package javaProject;
+
+public class arrayforeach {
+	public static void main(String[] args) {
+		  
+		   // create an array
+		   int[] age = {12, 4, 5};
+
+		   // loop through the array
+		   // using for loop
+		   System.out.println("Using for-each Loop:");
+		   for(int a : age) {
+		     System.out.println(a);
+		   }
+		 }
+}
---
+++ arrayloop.java
@@ -0,0 +1,14 @@
+package javaProject;
+
+public class arrayloop {
+	public static void main(String[] args) {
+		  
+		  
+		   int[] age = {12, 4, 5};
+
+		   System.out.println("Loop:");
+		   for(int i = 0; i < age.length; i++) {
+		     System.out.println(age[i]);
+		   }
+		 }
+}
