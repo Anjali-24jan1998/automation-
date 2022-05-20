8014313ea4b6ce7dcd75411a05d9709920b9b9a2
Anjali-24jan1998 <103317935+Anjali-24jan1998@users.noreply.github.com>
2022-05-20 13:16:52 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,16 @@
+package javaProject;
+
+public class string {
+	 public static void main(String[] args) {
+		    
+		    // create strings
+		    String first = "Java";
+		    String second = "Python";
+		    String third = "JavaScript";
+
+		    // print strings
+		    System.out.println(first);  
+		    System.out.println(second);  
+		    System.out.println(third);   
+		  }
+}
